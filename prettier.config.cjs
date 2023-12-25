a7db86bfccd530908e7a7ac8db6e2d0df7a05984
husush23 <husseinkadare40@gmail.com>
2023-12-26 00:04:06 +0530

    More styles added

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,4 @@
-// prettier.config.js
+/* eslint-disable-next-line */
 module.exports = {
   plugins: ["prettier-plugin-tailwindcss"],
 };
--- src/ui/Hero.jsx
+++ src/ui/Hero.jsx
@@ -1,22 +1,29 @@
 function Hero() {
   return (
-    <div className='mx-auto mt-4'>
-      <div className='flex flex-col px-8 py-2'>
-        <h1 className='text-4xl font-beVietnamPro'>
-          Hi, I am a <br />
-          <span className='font-plairFairDisplay'>
-            Visual designer <br />
-          </span>
-          <span className='font-poppins'>Based in Nairobi</span>
-        </h1>
+    <>
+      <div className="mx-auto mt-4 space-y-4 sm:flex sm:items-center">
+        <div className="mb-4 flex flex-col px-8 py-2">
+          <h1 className="text-4xl sm:text-8xl">
+            <span className="font-beVietnamPro font-semibold">
+              Hi. I am a <br />
+            </span>
+            <span className="font-plairFairDisplay sm:text-7xl">
+              full-stack developer <br />
+            </span>
+            <span className="font-poppins font-semibold">based in Nairobi</span>
+          </h1>
+        </div>
+        <img
+          src="./hussein2.png"
+          className="mx-auto mt-4 w-10/12 rounded-[30%] sm:h-4/5 sm:w-4/12"
+        />
       </div>
-      <img src='./hussein.png' />
-      <div className='social'>
+      <div className="social">
         <a>LinkedIn</a>
         <a>Facebook</a>
         <a>Behance</a>
       </div>
-    </div>
+    </>
   );
 }
 
