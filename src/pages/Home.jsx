c49f6b9daaf25c6bd0b360ea841c077b97c79840
husush23 <husseinkadare40@gmail.com>
2023-12-25 23:21:49 +0530

    Homepage created

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Hero from '../ui/Hero';
+import Navbar from '../ui/Navbar';
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
+  );
+}
