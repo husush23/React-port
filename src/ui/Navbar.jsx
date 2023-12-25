b1f70b63109757f557e4adc8e7aeb32ded83e9dd
husush23 <husseinkadare40@gmail.com>
2023-12-26 00:13:22 +0530

    IMage size edted

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,6 +1,6 @@
-import {useState} from 'react';
-import {CiMenuFries} from 'react-icons/ci';
-import {TfiClose} from 'react-icons/tfi';
+import { useState } from "react";
+import { CiMenuFries } from "react-icons/ci";
+import { TfiClose } from "react-icons/tfi";
 
 function Navbar() {
   const [hamburger, setHamburger] = useState(false);
@@ -11,9 +11,9 @@ function Navbar() {
 
   return (
     <nav>
-      <div className='hidden sm:flex items-center mt-0 justify-between px-4 py-4'>
-        <img src='./logo1.png' className='w-42 p-1 h-16' />
-        <ul className='hidden sm:flex items-center space-x-12 px-12 font-bold text-xl mr-16'>
+      <div className="mt-0 hidden items-center justify-between px-4 py-4 sm:flex">
+        <img src="./logo1.png" className="w-42 h-16 p-1" />
+        <ul className="mr-16 hidden items-center space-x-12 px-12 text-xl font-bold sm:flex">
           <li>
             <a>Home</a>
           </li>
@@ -25,23 +25,23 @@ function Navbar() {
           </li>
         </ul>
       </div>
-      <div className='sm:hidden'>
-        <div className='flex items-center justify-between'>
+      <div className="sm:hidden">
+        <div className="flex items-center justify-between">
           <a>
-            <img src='./logo1.png' alt='' className='w-42 p-1 h-16' />
+            <img src="./logo1.png" alt="" className="w-42 h-16 p-1" />
           </a>
-          <button onClick={onToggleMenu} className='px-4'>
+          <button onClick={onToggleMenu} className="px-4">
             {!hamburger ? <CiMenuFries /> : <TfiClose />}
           </button>
         </div>
         {hamburger && (
           <>
             <div
-              className='fixed inset-0 bg-black opacity-50 z-50'
+              className="fixed inset-0 z-50 bg-black opacity-50"
               onClick={onToggleMenu}
             ></div>
-            <ul className='fixed top-0 left-0 h-full w-64 bg-white z-50 p-4'>
-              {' '}
+            <ul className="fixed left-0 top-0 z-50 h-full w-64 bg-white p-4">
+              {" "}
               <li>
                 <a>Home</a>
               </li>
