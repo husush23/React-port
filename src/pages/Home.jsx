5a13f20757f22ef38ab335c972d819e8275c0e87
husush23 <husseinkadare40@gmail.com>
2023-12-26 20:40:46 +0530

    Home page Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import Hero from '../ui/Hero';
-import Navbar from '../ui/Navbar';
+import Hero from "../ui/Hero";
+import Info from "../ui/Info";
+import Navbar from "../ui/Navbar";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <Info />
     </main>
   );
 }
