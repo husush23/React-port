02575b7996960bced4746d9ad8ad1c23e81e1cfd
husush23 <husseinkadare40@gmail.com>
2023-12-28 18:40:56 +0530

    Gmail added

---
+++ src/components/Gmail.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Gmail() {
+  return (
+    <section className="my-48 ml-20  font-beVietnamPro">
+      <h3 className="text-3xl font-medium">Want to work together?</h3>
+      <h2 className="py-36 text-8xl font-semibold">
+        <Link to="mailto:your.email@example.com?subject=Subject%20Here&body=Message%20Here">
+          husush21@gmail.com
+        </Link>
+      </h2>
+    </section>
+  );
+}
+
+export default Gmail;
