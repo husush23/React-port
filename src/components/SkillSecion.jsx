1add06ddc901bb01259a72e755d67f331d47a4c1
husush23 <husseinkadare40@gmail.com>
2023-12-28 17:55:13 +0530

    Updated

--- src/components/SkillSecion.jsx
+++ src/components/SkillSecion.jsx
@@ -1,6 +1,6 @@
 function SkillSection({ children }) {
   return (
-    <div className="hover:shadow-right my-8 mr-2 border border-black px-4 py-4 hover:shadow-xl md:mx-8">
+    <div className="manual-shadow  my-8 mr-2 border border-black px-8 py-12 md:mx-8">
       {children}
     </div>
   );
