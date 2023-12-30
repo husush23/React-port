e80ad9d1e7d6ee0595bf615419ec5e208afdd23a
husush23 <husseinkadare40@gmail.com>
2023-12-30 22:24:25 +0530

    Mobile version of footer styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="mx-8 my-28 flex justify-between px-6">
+    <footer className="mx-4 my-8 space-y-8 text-sm md:mx-8 md:my-28 md:flex md:justify-between md:px-6 md:text-base">
       <div className="flex flex-col">
         <span>&copy; 2023 Hussein Hashi</span>
         <span className="pl-2">Full-stack developer</span>
       </div>
       <div>
-        <ul className="flex space-x-12 pr-44">
+        <ul className="flex space-x-6 pl-2 pr-44 md:space-x-12">
           <li>
             <Link>Behance</Link>
           </li>
@@ -25,3 +25,6 @@ function Footer() {
 }
 
 export default Footer;
+/*
+md:mx-8 md:my-28 md:flex md:justify-between md:px-6
+*/
