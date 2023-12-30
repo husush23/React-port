0f7bd28ba927f4bf7d0c3c5758e5d069b2b1f3a5
husush23 <husseinkadare40@gmail.com>
2023-12-30 22:26:54 +0530

    Work Page edited

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Projects from "../components/Projects";
 
 function Work() {
-  return <div>Works here</div>;
+  return (
+    <div className="container">
+      <Navbar />
+      <div className="mx-6 my-6 mb-4 text-4xl md:mx-8 md:my-28 md:text-8xl">
+        <h2 className="font-beVietnamPro font-semibold">Creating valuable</h2>
+        <h2 className="font-plairFairDisplay italic md:text-7xl">
+          software solutions
+        </h2>
+      </div>
+      <div className="mx-auto mt-7 px-6 md:mb-28">
+        <Projects />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Work;
