3f58487046cb7b4963bbd97bb99983b8557a7515
husush23 <husseinkadare40@gmail.com>
2023-12-31 17:31:13 +0530

    Social links added to contact page

--- src/components/Gmail.jsx
+++ src/components/Gmail.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Gmail() {
   return (
-    <section className="mx-10 my-16 font-beVietnamPro md:my-12  md:ml-20">
+    <section className="mx-10 my-12 font-beVietnamPro md:my-12  md:ml-20">
       <h2 className="w-[100%] text-wrap text-4xl font-semibold md:py-24 md:text-8xl md:font-semibold">
         <Link
           to="mailto:your.email@example.com?subject=Subject%20Here&body=Message%20Here"
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,27 @@
+import { Link } from "react-router-dom";
 import Gmail from "../components/Gmail";
 import Navbar from "../components/Navbar";
 
 function Contact() {
   return (
-    <div>
+    <section>
       <Navbar />
-      <div className="my-28">
+      <div className="my-16">
+        <div className="mx-10 flex flex-col space-y-8">
+          <h3 className="font-semibold">Send me a message</h3>
+          <Link className="text-sm font-medium">Click here</Link>
+        </div>
         <Gmail />
+        <div className="mx-10 my-28 flex flex-col space-y-10">
+          <h3>Or connect with me on socials</h3>
+          <ul className="flex flex-wrap text-3xl font-medium">
+            <li className="pr-8">Behance</li>
+            <li className="pr-8">Dribble</li>
+            <li className="pr-8">LinkedIn</li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
