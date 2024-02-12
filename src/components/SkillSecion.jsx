6e5b79b641afdad6152eb9f278743b741498bf76
husush23 <husseinkadare40@gmail.com>
2024-02-12 13:34:54 +0530

    Skills updates

--- src/components/SkillSecion.jsx
+++ src/components/SkillSecion.jsx
@@ -1,6 +1,6 @@
 function SkillSection({ children }) {
   return (
-    <div className="manual-shadow  my-8 mr-2 border border-black px-8 py-12 md:mx-8">
+    <div className="manual-shadow  mr-2space-y-2 my-8 border border-black px-8 py-12 md:mx-8">
       {children}
     </div>
   );
