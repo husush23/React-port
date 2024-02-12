8d8c2d934ec9a724cdea33e2bbdd803073a88fa8
husush23 <husseinkadare40@gmail.com>
2024-02-12 18:53:47 +0530

    Gmail updated

--- src/components/Gmail.jsx
+++ src/components/Gmail.jsx
@@ -5,7 +5,7 @@ function Gmail() {
     <section className="mx-10 my-12 font-beVietnamPro md:my-12  md:ml-20">
       <h2 className="w-[100%] text-wrap text-4xl font-semibold md:py-24 md:text-8xl md:font-semibold">
         <Link
-          to="mailto:your.email@example.com?subject=Subject%20Here&body=Message%20Here"
+          to="mailto:husush21@gmail.com?subject=Subject%20Here&body=Message%20Here"
           className="break-all underline"
         >
           husush21@gmail.com
