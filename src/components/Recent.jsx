13068cae312e2163f6fc0bf0fd7688a37d120ec3
husush23 <husseinkadare40@gmail.com>
2024-02-12 12:40:45 +0530

    Recent updated both logic and styles

--- src/components/Recent.jsx
+++ src/components/Recent.jsx
@@ -1,14 +1,10 @@
 import PropTypes from "prop-types";
 
-function Recent({ img, first, left, title, category }) {
+function Recent({ img, left, title, category }) {
   return (
-    <div
-      className={`space-y-4 py-12 ${
-        first ? "md:col-span-2 md:mx-8" : left ? "md:mt-14" : "md:mb-14"
-      }`}
-    >
-      <img src={img} alt={title} className="mx-auto w-full" />
-      <h2 className="text-2xl font-medium">{title}</h2>
+    <div className={`space-y-4 py-12 ${left ? "md:mt-14" : ""} mx-auto`}>
+      <img src={img} alt={title} className="project-shadow" />
+      <h2 className="mx-auto text-2xl font-medium">{title}</h2>
       <span className="text-slate-400">{category}</span>
     </div>
   );
@@ -20,6 +16,5 @@ Recent.propTypes = {
   img: PropTypes.string.isRequired,
   title: PropTypes.string,
   category: PropTypes.string,
-  first: PropTypes.bool,
   left: PropTypes.bool,
 };
