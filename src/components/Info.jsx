18303cd8255401f16d8b7325726ae43babc8bf56
husush23 <husseinkadare40@gmail.com>
2024-02-12 16:57:55 +0530

    info content updated

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -13,15 +13,17 @@ function Info() {
       </div>
       <div className="w-[25% mb-20 pt-4">
         <h2 className="md:-9 pb-8 text-lg font-medium md:text-3xl md:leading-[3rem]">
-          Passionate about visual design imperdiet nunc mauris turpis facilisis
-          suspendisse non. Purus viverra egestas amet sed pellentesque aliquet
-          pellentesque. Massa tincidunt sed nisl pulvinar congue.
+          Dedicated to full-stack development, I merge elegant design with
+          powerful functionality. Expert in crafting user-centric interfaces and
+          scalable backend systems. Passionate about delivering exceptional web
+          experiences through innovation and technology
         </h2>
         <p className="pb-6 text-lg">
-          Sed amet ultricies nibh in cras id in praesent vitae pellentesque
-          felis iaculis egestas interdum justo, lacus aenean in iaculis
-          scelerisque orci, dolor, consectetur blandit mi semper feugiat
-          consequat non volutpat varius.
+          Focused on end-to-end solutions, I ensure seamless integration of
+          front-end and back-end technologies. My approach combines technical
+          skill with design sensibility, aiming for both efficiency and visual
+          impact. Committed to excellence, I transform complex challenges into
+          intuitive and engaging digital products.
         </p>
         <Link className="border-b-2 border-cyan-400 pt-6 hover:border-black">
           More about me
