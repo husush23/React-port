640b08338d2946c1328666fd163c4333c4a2c314
Hussein Hashi <husseinkadare40@gmail.com>
2024-04-12 23:34:04 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="mx-4 my-8 space-y-8 text-sm md:mx-8 md:my-28 md:flex md:justify-between md:px-6 md:text-base">
       <div className="flex flex-col">
-        <span>&copy; 2023 Hussein Hashi</span>
+        <span>&copy; 2024 Hussein Hashi</span>
         <span className="pl-2">Full-stack developer</span>
       </div>
       <div>
